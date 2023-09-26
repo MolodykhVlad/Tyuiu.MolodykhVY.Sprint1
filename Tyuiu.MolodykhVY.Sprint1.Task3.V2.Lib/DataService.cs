f516b258bda19bf93aa234b82993e798b9664547
MolodykhVlad <Mr.Prins@yandex.ru>
2023-09-26 18:42:29 +0300

    Task3

---
+++ Tyuiu.MolodykhVY.Sprint1.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MolodykhVY.Sprint1.Task3.V2.Lib
+{
+    public class DataService : ISprint1Task2V2
+    {
+        public double ConvertAngleToRad(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Tyuiu.MolodykhVY.Sprint1.Task3.V2.Test/DataServiceTest.cs
+++ Tyuiu.MolodykhVY.Sprint1.Task3.V2.Test/DataServiceTest.cs
@@ -4,7 +4,7 @@ using System;
 namespace Tyuiu.MolodykhVY.Sprint1.Task3.V2.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class DataServiceTest
     {
         [TestMethod]
         public void TestMethod1()
